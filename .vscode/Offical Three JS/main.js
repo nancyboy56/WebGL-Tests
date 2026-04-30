018ec65aca2399661e6af01a3d0baa27a24c6e8e
Constantino <gooby5656@gmail.com>
2026-04-30 17:46:55 +1000

    trying to get intellisense to work

--- .vscode/Offical Three JS/main.js
+++ .vscode/Offical Three JS/main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // this is for displaying errors on the webpage
 window.onerror = function(message, source, lineno, colno, error) {
@@ -14,3 +16,40 @@ window.onerror = function(message, source, lineno, colno, error) {
 // install node.js
 //instakk three.js and vite
 // but can important from the web i think thats easier
+// have to import the gltfloader and orbitcontrols, not automatic
+
+
+main();
+
+// feel strange not writing my code in a fuciton
+// so ill put it in main
+function main() {
+  const scene = new THREE.Scene();
+
+  // should i keep them all as const?
+  // i feel like fov couldn change but ill leave for now
+  const fov = 75;
+
+  //ascept ratio
+  const aspect = window.innerWidth / window.innerHeight;
+
+  // other values can get you better performace
+  const near = 0.1;
+  const far = 1000;
+
+  //there are differnt kinda of cameras
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  const renderer = new THREE.WebGLRenderer();
+
+  // if you want to render at a lower resolution but keep the same siz in the browser
+  //renderer.setSize(window.innerWidth/2, window.innerHeight/2, false)
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+}
+
+function add_cube(){
+  const size = 1;
+  const box = new THREE.BoxGeometry(size,size,size);
+  const box_material = new THREE.Mes
+}

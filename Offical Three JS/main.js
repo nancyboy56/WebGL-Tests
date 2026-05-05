c683475ddcf367cb06779bc65d5261dbe51b262b
Constantino <gooby5656@gmail.com>
2026-05-05 12:31:40 +1000

    all in a function

--- Offical Three JS/main.js
+++ Offical Three JS/main.js
@@ -12,44 +12,49 @@ window.onerror = function (message, source, lineno, colno, error) {
     return false; // Prevents the error from showing in the console
 };
 
-const scene = new THREE.Scene();
-// should i keep them all as const?
-// i feel like fov couldn change but ill leave for now
-const fov = 75;
+function main() {
+    const scene = new THREE.Scene();
+    // should i keep them all as const?
+    // i feel like fov couldn change but ill leave for now
+    const fov = 75;
 
-//ascept ratio
-const aspect = window.innerWidth / window.innerHeight;
+    //ascept ratio
+    const aspect = window.innerWidth / window.innerHeight;
 
-// other values can get you better performace
-const near = 0.1;
-const far = 1000;
+    // other values can get you better performace
+    const near = 0.1;
+    const far = 1000;
 
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
 
-const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer();
 
-// if you want to render at a lower resolution but keep the same siz in the browser
-//renderer.setSize(window.innerWidth/2, window.innerHeight/2, false)
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    // if you want to render at a lower resolution but keep the same siz in the browser
+    //renderer.setSize(window.innerWidth/2, window.innerHeight/2, false)
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-const size = 1;
-const box = new THREE.BoxGeometry(size, size, size);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(box, material);
+    const size = 1;
+    const box = new THREE.BoxGeometry(size, size, size);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(box, material);
 
-scene.add(cube);
+    scene.add(cube);
 
-camera.position.z = 5;
+    camera.position.z = 5;
 
-const animate = function () {
-    requestAnimationFrame(animate);
+    const animate = function () {
+        requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
-};
+        renderer.render(scene, camera);
+    };
 
-animate();
+    animate();
+}
+
+main();
+
